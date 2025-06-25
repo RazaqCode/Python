11b11a6a0e57487b6297f5d36ee8ed334a23e049
RazaqCode <razaqrealm@gmail.com>
2025-06-25 21:58:02 +0530

    bool

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,18 +1,42 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        char_set = set()
-        left = 0
-        max_length = 0
+# Numeric Types
+int_var = 10
+float_var = 3.14
+complex_var = 2 + 3j
 
-        for right in range(len(s)):
-            while s[right] in char_set:
-                char_set.remove(s[left])
-                left += 1
-            char_set.add(s[right])
-            max_length = max(max_length, right - left + 1)
+# Text Type
+str_var = "Hello, Python!"
 
-        return max_length
+# Sequence Types
+list_var = [1, 2, 3]
+tuple_var = (4, 5, 6)
+range_var = range(5)
+
+# Set Types
+set_var = {7, 8, 9}
+frozenset_var = frozenset([10, 11, 12])
+
+# Mapping Type
+dict_var = {"name": "Razaq", "role": "Developer"}
+
+# Boolean Type
+bool_var = True
+
+# Binary Types
+bytes_var = b"Hello"
+bytearray_var = bytearray(3)
+memoryview_var = memoryview(bytes(5))
+
+# None Type
+none_var = None
+
+# Print value and its data type
+variables = [
+    int_var, float_var, complex_var, str_var,
+    list_var, tuple_var, range_var,
+    set_var, frozenset_var, dict_var,
+    bool_var, bytes_var, bytearray_var, memoryview_var,
+    none_var
+]
+
+for var in variables:
+    print(f"Value: {var} | Type: {type(var)}")
