0dd8488f9deec38029dad9743e2287eda7b3f678
RazaqCode <razaqrealm@gmail.com>
2025-06-25 22:06:38 +0530

    num

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,42 +1,21 @@
-# Numeric Types
-int_var = 10
-float_var = 3.14
-complex_var = 2 + 3j
+# Basic Python Data Types
+a = 10                   # int
+b = 3.14                 # float
+c = "Hello"              # str
+d = [1, 2, 3]            # list
+e = (4, 5, 6)            # tuple
+f = {7, 8, 9}            # set
+g = {"name": "Razaq"}    # dict
+h = True                 # bool
+i = None                 # NoneType
 
-# Text Type
-str_var = "Hello, Python!"
-
-# Sequence Types
-list_var = [1, 2, 3]
-tuple_var = (4, 5, 6)
-range_var = range(5)
-
-# Set Types
-set_var = {7, 8, 9}
-frozenset_var = frozenset([10, 11, 12])
-
-# Mapping Type
-dict_var = {"name": "Razaq", "role": "Developer"}
-
-# Boolean Type
-bool_var = True
-
-# Binary Types
-bytes_var = b"Hello"
-bytearray_var = bytearray(3)
-memoryview_var = memoryview(bytes(5))
-
-# None Type
-none_var = None
-
-# Print value and its data type
-variables = [
-    int_var, float_var, complex_var, str_var,
-    list_var, tuple_var, range_var,
-    set_var, frozenset_var, dict_var,
-    bool_var, bytes_var, bytearray_var, memoryview_var,
-    none_var
-]
-
-for var in variables:
-    print(f"Value: {var} | Type: {type(var)}")
+# Print type of each variable
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
