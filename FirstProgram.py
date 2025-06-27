71ec07d048f2a78b651a3f1edab3d208daff096d
RazaqCode <razaqrealm@gmail.com>
2025-06-27 22:14:09 +0530

    check

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,6 +1,6 @@
 # Check if a number is even or odd
 
-number = 7  # You can change this to any number
+number = 24  # You can change this to any number
 
 if number % 2 == 0:
     print(number, "is even")
