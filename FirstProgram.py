5aff1b391ab5b3f89bac4fd11a023b1938cfb7a6
RazaqCode <razaqrealm@gmail.com>
2025-07-02 20:27:05 +0530

    rev

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,7 +1,3 @@
-num = int(input("Enter a number: "))
-
-# Convert to string and check if same when reversed
-if str(num) == str(num)[::-1]:
-    print("It's a palindrome!")
-else:
-    print("Not a palindrome.")
+text = input("Enter a string: ")
+reversed_text = text[::-1]
+print("Reversed string:", reversed_text)
