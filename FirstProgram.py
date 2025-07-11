48e97a978e8ff8f191a982e24206bfcd9b0159ed
RazaqCode <razaqrealm@gmail.com>
2025-07-11 12:55:11 +0530

    hi

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,7 +1,1 @@
-print("10. Exception Handling")
-try:
-    value = 10 / 0
-except ZeroDivisionError as e:
-    print("Caught an exception:", e)
-finally:
-    print("This block always executes")
+print("hi, hello")
